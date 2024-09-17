bec2090b9cc9c2db7c1d0fe90622193b6261ca53
HoangTranNhat <trannhathoang17544@gmail.com>
2024-09-17 13:54:23 +0700

    OJT copy

---
+++ src/assets/index.tsx
@@ -0,0 +1,28 @@
+const images = {
+  common: {
+    kitchen_login: require('assets/images/kitchen_login.png'),
+    cashier_sidebar: require('assets/images/cashier_sidebar.png'),
+    avatar_default: require('assets/images/avatar_default.jpg'),
+  },
+  illustrations: {
+    kitchen_login: require('assets/illustrations/illustration_store.svg').default,
+    mbkc_cook: require('assets/illustrations/mbkc_cook.svg').default,
+    no_permission: require('assets/illustrations/illustration_403.svg').default,
+    not_found: require('assets/illustrations/illustration_404.svg').default,
+    server_error: require('assets/illustrations/illustration_500.svg').default,
+    empty_content: require('assets/illustrations/illustration_empty_content.svg').default,
+    otp: require('assets/illustrations/illustration_otp.svg').default,
+    email: require('assets/illustrations/illustration_email.svg').default,
+  },
+  logo: {
+    logo_mbkc: require('assets/logos/Cloud__4_-removebg-preview.png'),
+    logo_mbkc_no_bg: require('assets/logos/Cloud__4_-removebg-preview.png'),
+    logo_mbkc_text: require('assets/logos/z5836277677146_56f2df258c0283579d785df20e25d9e9.jpg'),
+  },
+  language: {
+    english: require('assets/icons/ic_flag_en.svg').default,
+    vietnamese: require('assets/icons/ic_flag_vn.svg').default,
+  },
+};
+
+export default images;
